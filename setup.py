f9350a8e25cbe631e597b89b6b770c65c2bae574
Jackson Augustus Fiore Hamburger <jhamburger@fb.com>
2020-07-09 14:01:24 -0400

    additional debug

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ build_ext = cmdclass.get('build_ext', build_ext)
 def build_library_files(dry_run, pic=False):
     cmd = [sys.executable, os.path.join(here_dir, 'ffi', 'build.py')]
     if pic:
+        print("Jackson debug confirmed pic")
         os.environ['CXXFLAGS'] = os.environ.get('CXXFLAGS', '') + ' -fPIC'
     spawn(cmd, dry_run=dry_run)
 
@@ -127,7 +128,7 @@ if bdist_wheel:
             # Ensure the binding file exist when running wheel build
             from llvmlite.utils import get_library_files
             # Turn on -fPIC for wheel building on Linux
-            print(sys.platform)
+            print("Jackson debug " + sys.platform)
             pic = sys.platform.startswith('linux')
             build_library_files(self.dry_run, pic=pic)
             self.distribution.package_data.update({
